840bb8575a64108ca9d6980aaeaf724eab32efa0
Surendra Kumar <surendrakumarmurugan@gmail.com>
2024-07-13 23:58:11 +0530

    Telegram link is added and some correction in the context is done successfully

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:["Backend Developer","Network Asprint","Web Developer"],
+    strings:["Backend Developer","Network Asprint","Web Developer","Data Analyst"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
